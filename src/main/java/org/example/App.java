4f9860638dbf5b86890232c91e43817d3c036822
Nilay <mailnilaydas@gmail.com>
2022-07-27 15:00:37 +0530

    2nd commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome To product Application" );
+        System.out.println("jenkin is cool");
     }
 }
